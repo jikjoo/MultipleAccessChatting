17f26277309904d590dcf1047cddac4ba29b4d82
jikjoo <pillast777@naver.com>
2019-11-08 21:10:51 +0900

    initial

---
+++ client.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void socket(){
+
+}
+
+void connect(){
+
+}
+
+void read(){
+
+}
+
+void write(){
+
+}
+
+void close(){
+
+}
+
+int main(void){
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void socket(){
+
+}
+
+void bind(){
+
+}
+
+void listen(){
+
+}
+
+void accept(){
+
+}
+
+void read(){
+
+}
+
+void write(){
+
+}
+
+void close(){
+
+}
+
+int main(void){
+    
+    return 0;
+}
