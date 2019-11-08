73640e6da3e15e1544296c19c75a202f8459f8de
jikjoo <pillast777@naver.com>
2019-11-08 22:03:56 +0900

    raw test

--- client.c
+++ client.c
@@ -3,19 +3,22 @@
 #include <pthread.h>
 
 void socket(){
-
+    printf("open!! client\n");
 }
 
 void connect(){
+    printf("Success to connect to server!\n");
 
 }
 
 void read(){
-
+    printf("Server : welcome to chatting server!!\n");
 }
 
 void write(){
-
+    //printf(" Chatting On...\n");
+    char msg[2000];
+    scanf("%s",msg);
 }
 
 void close(){
@@ -23,5 +26,13 @@ void close(){
 }
 
 int main(void){
+    int ip[4] = {127,0,0,1};
+    int port = 1234;
+    printf("Input Server IP Address : %d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]);
+    printf("Input Server Port Number : %d",port);
+    socket();
+    connect();
+    read();
+    write();
     return 0;
 }
--- server.c
+++ server.c
@@ -4,26 +4,25 @@
 
 void socket(){
 
+    printf("open!! server\n");
+
 }
 
 void bind(){
 
+    printf("Chatting on\n");
 }
 
-void listen(){
-
+void listen(int client_num){
 }
 
-void accept(){
-
-}
-
-void read(){
+void accept(int client_num){
+    pritnf("connected to Client %d\n", client_num);
 
 }
 
-void write(){
-
+void read_write(int client_num, char *msg){
+    printf("message from client %d : %s\n",client_num,msg);
 }
 
 void close(){
@@ -31,6 +30,12 @@ void close(){
 }
 
 int main(void){
-    
+    int port = 30;
+    printf("Server Port : %d\n",port);
+    socket();
+    bind();
+    listen(1);
+    accept(1);
+    read_write(1,"hi");
     return 0;
 }
