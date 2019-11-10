bf6cf2db4680c31904aea9f58f4b3d957d2092b4
jikjoo <pillast777@naver.com>
2019-11-10 20:16:18 +0900

    finish

--- client.c
+++ client.c
@@ -1,38 +1,118 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
 
-void c_socket(){
+#define PORT 1234
+#define MLIM 128
+
+void* c_write();
+void* c_read();
+
+pthread_mutex_t mutexsum = PTHREAD_MUTEX_INITIALIZER;  
+int socket_num;
+
+int main(int argc,char *argv[]){
+    struct sockaddr_in server;
+    char buffer[128];
+    char *msg;
+    int close = 0,check;
+    socket_num = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_num <= 0){
+        printf("fail!! to create socket");
+        return -1;
+    }
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    // convert IP addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &server.sin_addr)<=0){
+        printf("fail!! Invalid IP address\n");
+        return -1;
+    }
+    //server.sin_addr.s_addr = htonl(INADDR_ANY);
+    printf("Input Server IP Address : %s\n","127.0.0.1");
+    printf("Input Server Port Number : %d\n",PORT);
     printf("open!! client\n");
-}
 
-void c_connect(){
+    if (connect(socket_num, (struct sockaddr *)&server, sizeof(server)) < 0) 
+    { 
+        printf("\nfail!! to connect Server\n"); 
+        return -1; 
+    } 
     printf("Success to connect to server!\n");
+    /* while(1){
+        
+        bzero(buffer,MLIM);
+        check = read(socket_num,buffer,MLIM);
+        buffer[MLIM] = '\0';
+        if(check <= 0){
+            printf("fail to read %d\n", check);
+        }
+        printf("%s\n",buffer);
 
-}
+        bzero(buffer,MLIM);
+        printf("Message to server: ");
+        scanf("%s", buffer);
+        printf("writing...%d\n",socket_num);
+        check = write(socket_num,buffer,strlen(buffer));
+        if(check <= 0){
+            printf("fail to write %d\n",check);
+        }   
+    } */
+    
 
-void c_read(){
-    printf("Server : welcome to chatting server!!\n");
-}
-
-void c_write(){
-    //printf(" Chatting On...\n");
-    char msg[2000];
-    scanf("%s",msg);
-}
-
-void c_close(){
-
-}
-
-int main(void){
-    int ip[4] = {127,0,0,1};
-    int port = 1234;
-    printf("Input Server IP Address : %d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]);
-    printf("Input Server Port Number : %d",port);
-    c_socket();
-    c_connect();
-    c_read();
-    c_write();
+    //setup threads for read and write
+    pthread_t threads[2];
+    pthread_create(&threads[0],NULL,c_write,NULL);
+    pthread_create(&threads[1],NULL,c_read,NULL);
+   
+    pthread_join(threads[0],NULL);
+    pthread_join(threads[1],NULL);
+    printf("Close\n");
     return 0;
+}
+
+void *c_write(){
+    char msg[MLIM];
+    int s;
+    while(1){
+        bzero(msg,MLIM);
+        scanf("%[^\n]%*c",msg);
+        //printf("writing...%d\n",socket_num);
+        if(s = send(socket_num,msg,strlen(msg),0)<0){
+            printf("fail!! to send message\n");
+            break;
+        }
+        else if(strcmp(msg,"Q") == 0){
+            close(socket_num);
+            break;
+        }
+        //printf("writing done\n");
+        bzero(msg,MLIM);
+    }
+}
+
+void *c_read(){
+    char msg[MLIM];
+    int r;
+    while(1){
+        //printf("reading...\n");
+        bzero(msg,MLIM);
+        if((r = recv(socket_num,msg,MLIM,0))<0){
+            printf("fail!! to read message");
+            close(socket_num);
+            break;
+        }
+        else if(r==0){
+            close(socket_num);
+            break;
+        }
+        else if(strcmp(msg, "Server : Welcome to chatting server!!") == 0){
+            printf("  Chatting On...\ninput 'Q' to exit\n");
+        }
+        printf("%s\n",msg);
+    }
 }
--- server.c
+++ server.c
@@ -1,41 +1,167 @@
-#include <stdio.h>
-#include <unistd.h>
+//gcc -c server.c && gcc -pthread -o server.o server.c && gcc -c client.c && gcc -pthread -o client.o client.c
+#include <netinet/in.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-void s_socket(){
+#define MAX_CLIENT 2
+#define PORT 1234
+#define MLIM 128 //message limit
 
-    printf("open!! server\n");
+void *read_write(void *);
+int clientCount = 0;
+struct clientInfo {
+	int index;
+	int client_num;
+};
+struct clientInfo ClientInfo[MAX_CLIENT];
 
+//for mutex
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+int main(int argc, char *argv[]) {
+	//prepare server
+	struct sockaddr_in server, client;
+	int addrlen = sizeof(server);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(PORT);
+	int socket_num, client_num, Index = 0;
+	//purchase phone
+	printf("Server Port : %d\n", PORT);
+	socket_num = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_num <= 0) {
+		printf("fail!! to open server\n");
+	}
+	printf("open!! server\n");
+
+	//assigned phone number
+	int b = bind(socket_num, (struct sockaddr *)&server, sizeof(server));
+	if (b < 0) {
+		printf("fail!! to bind\n");
+		return 1;
+	}
+	printf("Chatting on\n");
+	//listening
+	listen(socket_num, MAX_CLIENT);
+
+	// pick up the phone
+	pthread_t thread_id[MAX_CLIENT];
+	int clilen = sizeof(client);
+	while (client_num = accept(socket_num, (struct sockaddr *)&client, (socklen_t *)&clilen)) {
+		int pos = 0; // find empty in ClientInfo
+		if (clientCount < MAX_CLIENT) {
+			pthread_mutex_lock(&mtx);
+			for (pos = 0; pos < MAX_CLIENT; pos++) {
+				if (ClientInfo[pos].client_num == 0 && ClientInfo[pos].index == 0)
+					break;
+			}
+			ClientInfo[pos].client_num = client_num;
+			ClientInfo[pos].index = ++Index;
+			pthread_mutex_unlock(&mtx);
+		} else {
+			printf("fail!! full client\n");
+			continue;
+		}
+		// make thread for accepted Clients
+		if (pthread_create(&thread_id[pos], NULL, read_write, (void *)&ClientInfo[pos])) {
+			printf("fail!! to create thread\n");
+			return 1;
+		}
+		clientCount++;
+	}
+	/* for (int i = 0; i < MAX_CLIENT; i++)
+		pthread_join(thread_id[i], NULL); */
+
+	if (client_num < 0) {
+		printf("fail!! to accept\n");
+		return 1;
+	}
+	printf("Close");
+	return 0;
 }
 
-void s_bind(){
+void *read_write(void *clientDetail) {
+	// client socket
+	pthread_mutex_lock(&mtx);
+	struct clientInfo *ClientDetail = (struct clientInfo *)clientDetail;
+	int c_socket = ClientDetail->client_num;
+	int index = ClientDetail->index;
+	//int c_socket = *(int *)client_num;
+	int read_size = 0;
+	char buffer[MLIM];
+	char msg[MLIM + 10];
+    int closed = 0;
 
-    printf("Chatting on\n");
-}
+	printf("connected to Client %d\n", index);
+	// send message to client
+	// critical zone
+	for (int i = 0; i < MAX_CLIENT; i++) {
+		if (ClientInfo[i].client_num == 0)
+			continue;
+		else if (ClientInfo[i].client_num == c_socket) {
+			bzero(buffer, MLIM);
+			strcpy(buffer, "Server : Welcome to chatting server!!");
+		} else {
+			bzero(buffer, MLIM);
+			sprintf(buffer, "Client %d has been connected!", index);
+		}
+		if (send(ClientInfo[i].client_num, buffer, strlen(buffer), 0) < 0) {
+			printf("fail! to send message to %d\n", ClientInfo[i].index);
+			clientCount--;
+			ClientDetail->client_num = 0;
+			ClientDetail->index = 0;
+			close(c_socket);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&mtx);
 
-void s_listen(int client_num){
-}
-
-void s_accept(int client_num){
-    pritnf("connected to Client %d\n", client_num);
-
-}
-
-void read_write(int client_num, char *msg){
-    printf("message from client %d : %s\n",client_num,msg);
-}
-
-void s_close(){
-
-}
-
-int main(void){
-    int port = 30;
-    printf("Server Port : %d\n",port);
-    s_socket();
-    s_bind();
-    s_listen(1);
-    s_accept(1);
-    read_write(1,"hi");
-    return 0;
+	// receive message from client
+	bzero(buffer, MLIM);
+	bzero(msg, MLIM + 10);
+	while ((read_size = recv(c_socket, buffer, MLIM, 0) > 0)) {
+		pthread_mutex_lock(&mtx);
+		// critical zone
+		buffer[MLIM] = '\0';
+		printf("message from client %d : %s\n", index, buffer);
+		// Close Client n
+		if (strcmp(buffer, "Q") == 0) {
+            closed = 1;
+            sprintf(msg, "Client %d disconnected",index);
+		}
+		else sprintf(msg, "Client %d : %s", index, buffer);
+		// echo to other clients
+		for (int i = 0; i < MAX_CLIENT; i++) {
+			if (ClientInfo[i].client_num == 0 || ClientInfo[i].client_num == c_socket) {
+				continue;
+			}
+			//printf("sending...\n");
+			if (send(ClientInfo[i].client_num, msg, strlen(msg), 0) < 0) {
+				printf("fail! to send message to %d\n", ClientInfo[i].index);
+				clientCount--;
+				ClientDetail->client_num = 0;
+				ClientDetail->index = 0;
+				close(c_socket);
+				break;
+			}
+		}
+		bzero(buffer, MLIM);
+		bzero(msg, MLIM + 10);
+        if(closed){
+            ClientDetail->client_num = 0;
+            ClientDetail->index = 0;
+            clientCount--;
+            printf("disconnected Client %d\n", index);
+            close(c_socket);
+            pthread_mutex_unlock(&mtx);
+            break;
+        }
+		pthread_mutex_unlock(&mtx);
+	}
+	if (read_size < 0) {
+		printf("fail!! to receive %d %d\n", read_size, c_socket);
+	}
 }
